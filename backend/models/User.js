0f63d8bea67f15280cdfc851ad4e124f15e4a1ed
tylerc122 <tylercollo1@gmail.com>
2024-08-10 10:15:38 -0600

    Updated comments.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,10 @@
+/// Model for user
+
 const mongoose = require('mongoose');
 
+// Each user must have a username, and a password.
+// The user must be unique as we don't want to deal with multiple users of the same name.
+// Though, every password need not be uniqe.
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },

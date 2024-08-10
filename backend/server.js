377509216bb0037114dc17d31fbae8414c727215
tylerc122 <tylercollo1@gmail.com>
2024-08-10 10:20:43 -0600

    Updated comments

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use('/api/auth', authRoutes);
 
+// Server running on port 5001.
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
     .on('error', (error) => {

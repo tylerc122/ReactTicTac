8206ce900b57d12830c37e94feacd87739aa9a78
tylerc122 <tylercollo1@gmail.com>
2024-08-12 10:18:42 -0600

    Started implementing offline mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './Login';
 import Game from './Game';
 import Navbar from './Navbar';
@@ -7,11 +7,19 @@ import './styles.css';
 
 function App() {
   const { user } = useAuth();
+  const [isOfflineMode, setIsOfflineMode] = useState(false);
+
+  const toggleOfflineMode = () => {
+    setIsOfflineMode(!isOfflineMode);
+  }
 
   return (
     <div className="App">
       <Navbar />
       <div style={{ marginTop: '10px' }}></div>
+      <button onClick={toggleOfflineMode}>
+        {isOfflineMode ? 'Switch to Online Mode' : 'Switch to Offline Mode'}
+      </button>
       {user ? <Game /> : <Login />}
     </div>
   );

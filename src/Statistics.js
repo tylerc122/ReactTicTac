b8471229fb5975586ef9efcaabd8eba231a96590
tylerc122 <tylercollo1@gmail.com>
2024-09-10 14:32:22 -0400

    Removed ununused variables

--- src/Statistics.js
+++ src/Statistics.js
@@ -6,15 +6,16 @@ import { useAuth } from './AuthContext';
 const Statistics = () => {
     const { user } = useAuth();
 
+    // If in offline mode or user cannot be initialized, user stats cannot be initialized
     if (!user || !user.stats) {
         return <div>No statistics available</div>
     }
 
-    const { wins, losses, draws } = user.stats;
+    // Calculating total games and win %.
     const totalGames = user.stats.wins + user.stats.losses + user.stats.draws;
     const winPercentage = totalGames > 0 ? ((user.stats.wins / totalGames) * 100).toFixed(2) : 0;
 
-
+    // Return stats formatted to display on the app.
     return (
         <div className="statistics">
             <h2>Player Statistics</h2>

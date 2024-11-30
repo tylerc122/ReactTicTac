615559ec394fdabbcc15e32d94aaf1a8ecb57146
tylerc122 <tylercollo1@gmail.com>
2024-11-30 15:58:16 -0500

    Fixed a bug where stats wouldn't close after swtich to offline mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 /// Defines how all files interact with one another.
 
-import React, { useState } from 'react';
-import Login from './Login';
-import Game from './Game';
-import Navbar from './Navbar';
-import { useAuth } from './AuthContext';
-import './styles.css';
-import Statistics from './Statistics';
-import { Button } from '@mui/material';
+import React, { useState } from "react";
+import Login from "./Login";
+import Game from "./Game";
+import Navbar from "./Navbar";
+import { useAuth } from "./AuthContext";
+import "./styles.css";
+import Statistics from "./Statistics";
+import { Button } from "@mui/material";
 
 function App() {
   const { user } = useAuth();
@@ -17,32 +17,47 @@ function App() {
   const toggleOfflineMode = () => {
     setIsOfflineMode(!isOfflineMode);
     setOfflineGameType(null);
-  }
+  };
 
   const selectOfflineGameType = (type) => {
     setOfflineGameType(type);
-  }
+  };
 
   return (
     <div className="App">
       <Navbar />
-      <div style={{ marginTop: '80px' }}></div>
+      <div style={{ marginTop: "80px" }}></div>
       <Button onClick={toggleOfflineMode}>
-        {isOfflineMode ? 'Switch to Online Mode' : 'Switch to Offline Mode'}
+        {isOfflineMode ? "Switch to Online Mode" : "Switch to Offline Mode"}
       </Button>
-      {user && <Statistics />}
+      {user && !isOfflineMode && <Statistics />}
       {isOfflineMode ? (
         <>
           {!offlineGameType && (
             <div>
-              <Button variant= "contained" onClick={() => selectOfflineGameType('local')}>Play with a Friend</Button>
-              <Button variant= "contained" sx={{ml: '1%'}}onClick={() => selectOfflineGameType('bot')}>Play Against Bot</Button>
+              <Button
+                variant="contained"
+                onClick={() => selectOfflineGameType("local")}
+              >
+                Play with a Friend
+              </Button>
+              <Button
+                variant="contained"
+                sx={{ ml: "1%" }}
+                onClick={() => selectOfflineGameType("bot")}
+              >
+                Play Against Bot
+              </Button>
             </div>
           )}
-          {offlineGameType && <Game isOfflineMode={true} offlineGameType={offlineGameType} />}
+          {offlineGameType && (
+            <Game isOfflineMode={true} offlineGameType={offlineGameType} />
+          )}
         </>
+      ) : user ? (
+        <Game isOfflineMode={false} offlineGameType={null} />
       ) : (
-        user ? <Game isOfflineMode={false} offlineGameType={null} /> : <Login />
+        <Login />
       )}
     </div>
   );
